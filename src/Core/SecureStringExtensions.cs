6d234407e866b53e8560ca99fbffe1937d0d94dc
Pavels Sergijenko <pavels.sergijenko@if.lv>
2019-07-29 15:01:42 +0300

    Fixed ToByteArray method.

--- src/Core/SecureStringExtensions.cs
+++ src/Core/SecureStringExtensions.cs
@@ -20,41 +20,29 @@ namespace Security.HMAC
                 .Aggregate(new SecureString(), AppendChar, MakeReadOnly);
         }
 
-        public static unsafe byte[] ToByteArray(this SecureString secStr, Encoding encoding)
+        public static byte[] ToByteArray(this SecureString secStr, Encoding encoding)
         {
-            if (secStr == null)
+            if (secStr == null || secStr.Length == 0)
             {
                 return new byte[0];
             }
 
-            int strLen = secStr.Length;
-            int bufferSz = encoding.GetMaxByteCount(strLen);
-
-            IntPtr bytes = IntPtr.Zero;
-            IntPtr str = IntPtr.Zero;
-
+            IntPtr ptr = IntPtr.Zero;
             try
             {
-                bytes = Marshal.AllocHGlobal(bufferSz);
-                str = Marshal.SecureStringToBSTR(secStr);
+                ptr = Marshal.SecureStringToGlobalAllocUnicode(secStr);
+                var result = Marshal.PtrToStringUni(ptr);
+                if (result == null)
+                    throw new Exception("Failed to retrieve security string.");
 
-                char* chars = (char*)str.ToPointer();
-                byte* bptr = (byte*)bytes.ToPointer();
-                int len = encoding.GetBytes(chars, strLen, bptr, bufferSz);
-
-                byte[] arr = new byte[len];
-                Marshal.Copy((IntPtr)bptr, arr, 0, len);
-
-                // zero buffers
-                Zero((IntPtr)chars, strLen);
-                Zero((IntPtr)bptr, len);
-
-                return arr;
+                return encoding.GetBytes(result);
             }
             finally
             {
-                if (str != IntPtr.Zero) Marshal.ZeroFreeBSTR(str);
-                if (bytes != IntPtr.Zero) Marshal.FreeHGlobal(bytes);
+                if (ptr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeGlobalAllocUnicode(ptr);
+                }
             }
         }
 
